66db18cb97a5a12f0cf7400d8c01d6a5ce1a7847
beacon-global <beaconmcglobal@gmail.com>
2024-12-21 10:11:22 +0000

    script update

--- src/app/layout.js
+++ src/app/layout.js
@@ -79,7 +79,7 @@ export default function RootLayout({ children }) {
         <meta property="og:image:width" content="1024" />
         <meta property="og:image:height" content="1024" />
 
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -89,7 +89,18 @@ export default function RootLayout({ children }) {
               })(window,document,'script','dataLayer','GTM-W2WWJ3TG');
             `,
           }}
-        ></script>
+        ></script> */}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W2WWJ3TG');
+          `}
+        </Script>
       </head>
       <body className={Sora.className}>
         <noscript>
